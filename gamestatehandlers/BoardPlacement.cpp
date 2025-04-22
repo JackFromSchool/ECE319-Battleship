e78be263312cc35f9eff1ee2f858d166e08ce9de
5hrey45-Del <shreyas.gupta1026@gmail.com>
2025-04-22 00:58:13 -0500

    finished code for board placement

--- gamestatehandlers/BoardPlacement.cpp
+++ gamestatehandlers/BoardPlacement.cpp
@@ -1,13 +1,13 @@
 #include "BoardPlacement.h"
 #include "Events.h"
-#include "Globals.h"
-/*
+
 void initBoardPlacement() {
-    ST7735_DrawBitmap(0, 160, battleship_board, 128, 160);
+    engineState.isPlayer1Turn = true;
+    engineState.player1.drawMyBoard();
 }
 
 enum GameState handleBoardPlacement(enum Event event) {
-    if(event == BUTTON0_PRESS || event == BUTTON0_UNPRESS || event == BUTTON1_PRESS || event == BUTTON1_UNPRESS || event == BUTTON2_PRESS || event == BUTTON2_UNPRESS || event == BUTTON3_PRESS || event == BUTTON3UNPRESS)
+    if(event == BUTTON0_PRESS || event == BUTTON0_UNPRESS || event == BUTTON1_PRESS || event == BUTTON1_UNPRESS || event == BUTTON2_PRESS || event == BUTTON2_UNPRESS)
     {
         return;
     }
@@ -95,8 +95,30 @@ enum GameState handleBoardPlacement(enum Event event) {
                         engineState.player1.five_ship.board_pos_x++;
                         break;
                 }
+            case(BUTTON3_PRESS)
+            {
+                switch(engineState.player1.numShipsPlaced)
+                {
+                    case 0:
+                        engineState.player1.two_ship0.isPlaced = true;
+                        break;
+                    case 1:
+                        engineState.player1.two_ship1.isPlaced = true;
+                        break;
+                    case 2:
+                        engineState.player1.three_ship.isPlaced = true;
+                        break;
+                    case 3:
+                        engineState.player1.four_ship.isPlaced = true;
+                        break;
+                    case 4:
+                        engineState.player1.five_ship.isPlaced = true;
+                        break;
+                }
+                engineState.player1.numShipsPlaced++;
+            }
         }
-        engineState.player1.drawMyBoard;
+        engineState.player1.drawMyBoard();
     }
     else 
     {
@@ -104,7 +126,7 @@ enum GameState handleBoardPlacement(enum Event event) {
         {
             case(JOYSTICK_DOWN)
                 //Get the object for Player 1, move the head of the ship in the specified direction
-                switch(engineState.player1.numShipsPlaced)
+                switch(engineState.player2.numShipsPlaced)
                 {
                     case 0:
                         engineState.player2.two_ship0.board_pos_y--;
@@ -124,7 +146,7 @@ enum GameState handleBoardPlacement(enum Event event) {
                 }
             case(JOYSTICK_UP)
                 //Get the object for Player 1, move the head of the ship in the specified direction
-                switch(engineState.player1.numShipsPlaced)
+                switch(engineState.player2.numShipsPlaced)
                 {
                     case 0:
                         engineState.player2.two_ship0.board_pos_y++;
@@ -144,7 +166,7 @@ enum GameState handleBoardPlacement(enum Event event) {
                 }
             case(JOYSTICK_LEFT)
                 //Get the object for Player 1, move the head of the ship in the specified direction
-                switch(numShipsPlaced)
+                switch(engineState.player2.numShipsPlaced)
                 {
                     case 0:
                         engineState.player2.two_ship0.board_pos_x--;
@@ -164,7 +186,7 @@ enum GameState handleBoardPlacement(enum Event event) {
                 }
             case(JOYSTICK_RIGHT)
                 //Get the object for Player 1, move the head of the ship in the specified direction
-                switch(numShipsPlaced)
+                switch(engineState.player2.numShipsPlaced)
                 {
                     case 0:
                         engineState.player2.two_ship0.board_pos_x++;
@@ -182,9 +204,31 @@ enum GameState handleBoardPlacement(enum Event event) {
                         engineState.player2.five_ship.board_pos_x++;
                         break;
                 }
+            case(BUTTON3_PRESS)
+            {
+                switch(engineState.player2.numShipsPlaced)
+                {
+                    case 0:
+                        engineState.player2.two_ship0.isPlaced = true;
+                        break;
+                    case 1:
+                        engineState.player2.two_ship1.isPlaced = true;
+                        break;
+                    case 2:
+                        engineState.player2.three_ship.isPlaced = true;
+                        break;
+                    case 3:
+                        engineState.player2.four_ship.isPlaced = true;
+                        break;
+                    case 4:
+                        engineState.player2.five_ship.isPlaced = true;
+                        break;
+                }
+                engineState.player2.numShipsPlaced++;
+            }
         }
-        engineState.player2.drawMyBoard;
+        engineState.player2.drawMyBoard();
     }
 }
-*/
+
 //update the board's placement and continuously refresh the board
